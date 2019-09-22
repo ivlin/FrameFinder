26ad4f83970d3b0f1ad8a927e132bd130eecbfa3
minivan <ivlin112@gmail.com>
2019-09-22 17:39:15 -0400

    removed relevant lines

--- app.py
+++ app.py
@@ -93,9 +93,6 @@ def clear():
 
 if __name__=="__main__":
     app.debug = True
-    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
     app.run(host='0.0.0.0', port=8000, use_reloader=True)
 
 if __name__ != ‘__main__’:
