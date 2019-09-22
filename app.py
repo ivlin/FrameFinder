ed196320bee3f29be2245af9a7eedc52d1cd57a5
minivan <ivlin112@gmail.com>
2019-09-22 17:36:32 -0400

    moved logging logic

--- app.py
+++ app.py
@@ -97,3 +97,8 @@ if __name__=="__main__":
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
     app.run(host='0.0.0.0', port=8000, use_reloader=True)
+
+if __name__ != ‘__main__’:
+    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
