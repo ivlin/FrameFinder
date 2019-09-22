40c4715576616a929778267fa54906941183cd42
minivan <ivlin112@gmail.com>
2019-09-22 17:43:30 -0400

    moved relevant lines

--- app.py
+++ app.py
@@ -91,11 +91,11 @@ def clear():
         pass
     return redirect(url_for('index'))
 
+if __name__!="__main__":
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
 if __name__=="__main__":
     app.debug = True
     app.run(host='0.0.0.0', port=8000, use_reloader=True)
-
-if __name__ != ‘__main__’:
-    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
