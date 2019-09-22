f86bf5ea95f37bb3147542489e7eafff90c54044
minivan <ivlin112@gmail.com>
2019-09-22 16:17:12 -0400

    doesnt need a database, modified confid

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
-
 
 class ProductionConfig(Config):
     DEBUG = False
