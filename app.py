d6df62055f595fb7e5a1717482d627a1a8592a30
minivan <ivlin112@gmail.com>
2019-09-23 00:43:26 -0400

    mroe debugging

--- app.py
+++ app.py
@@ -8,10 +8,86 @@ from rq import Queue
 from rq.job import Job
 from worker import conn
 
+import cv2, numpy, skimage
+import time, math
+
+
 app = Flask(__name__)
 app.config.from_object(os.environ["APP_SETTINGS"])
 
 q=Queue(connection=conn)
+
+def get_distance(target, candidate):
+    target = ~cv2.Canny(target, 100, 100)
+    candidate = ~cv2.Canny(candidate, 100, 100)
+    return numpy.linalg.norm(target-candidate) + \
+        skimage.measure.compare_nrmse(target, candidate) + \
+        skimage.measure.compare_ssim(target, candidate)
+
+#last dist is used to prevent consecutive frames from being extracted
+def update_order(ordering, dist, val, name, last_dist, cut_margin=0.005,fps=30):
+    if ordering[-1] and dist > ordering[-1][0]:
+        return False
+    else:
+        for i in range(len(ordering)):
+            if (ordering[i] is None or ordering[i][0]>dist) and \
+                    (last_dist is None or 1.0*abs(last_dist-dist)/last_dist > cut_margin):
+                #cv2.imwrite("candidate.png",~cv2.Canny(val,100,100))
+                ordering.insert(i,(dist,"%06d"%name ,name/fps,val,~cv2.Canny(val, 100, 100)))
+                ordering.pop()
+                return True
+        return False
+
+
+#https://stackoverflow.com/questions/30136257/how-to-get-image-from-video-using-opencv-python
+def extract_top_frames(video, path_output_dir, target, top_n, pulls_per_second=2):
+    ordering=[None]*top_n
+
+    vidcap = cv2.VideoCapture(video)
+    fps = vidcap.get(cv2.CAP_PROP_FPS);
+
+    cur_frame = 0
+    extracted_frames = 0
+
+    #ensure no consecutive frames are returned
+    last_dist = None
+
+    while vidcap.isOpened():
+        success = vidcap.grab()
+        if success and cur_frame % (math.ceil(fps/pulls_per_second)) == 0:
+            success, image = vidcap.retrieve()
+            if extracted_frames==0:
+                h,w,d = image.shape
+                target = cv2.resize(target,(int(w),int(h)))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            if success:
+                dist = get_distance(image, target)
+                update_order(ordering, dist, image, cur_frame, last_dist)
+                extracted_frames += 1
+            last_dist = dist
+            cur_frame +=1
+        elif success:
+            cur_frame += 1
+        else:
+            break
+    cv2.destroyAllWindows()
+    vidcap.release()
+    return ordering
+
+def run_extractor(target_img, video, path_output_dir, num_results=10):
+    app.logger.debug(target_img)
+    app.logger.debug(oslistdir("/static/in"))
+    target = cv2.imread(target_img)
+    target = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
+    top_n_frames = extract_top_frames(video, "out", target, num_results)
+
+    for img_ind in range(len(top_n_frames)):
+        if top_n_frames[img_ind] is None:
+            break
+        #print ("Rank %d: Frame %s with distance %f"%(img_ind, top_n_frames[img_ind][1], top_n_frames[img_ind][0]))
+        cv2.imwrite(os.path.join(path_output_dir, '%s.png'%(top_n_frames[img_ind][1])), top_n_frames[img_ind][3])
+        cv2.imwrite(os.path.join(path_output_dir, '%s_proc.png'%(top_n_frames[img_ind][1])), top_n_frames[img_ind][4])
+    return top_n_frames
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['png','jpg','jpeg']
@@ -47,7 +123,7 @@ def youtube():
         except:
             pass
         app.logger.debug("accessing/static/in/%s"%(file.filename))
-        job = q.enqueue_call(func = similar_engine.run_extractor, \
+        job = q.enqueue_call(func = run_extractor, \
             args=("/static/in/%s"%(file.filename), "videos/%s.mp4"%url_ext, "static/out/%s"%url_ext))
         return redirect(url_for("get_results",job_key=job.get_id(),ext=url_ext,target=file.filename))
         #return render_template("wait.html",joburl="http://localhost:8000/results/%s/%s/%s"%(str(job.get_id()), url_ext, file.filename))
